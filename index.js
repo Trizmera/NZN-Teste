f2bab3637e528c0c677245429bf43ab5b0773cbd
Beatriz Cruz <mllebia@live.com>
2024-02-19 12:29:12 -0300

    funcao de busca

---
+++ index.js
@@ -0,0 +1,19 @@
+function buscaLinearAlvo(lista, alvo)
+{
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] === alvo) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+const listaNum = [-1, 0, 3, 5, 9, 12];
+const numAlvo = 2;
+const index = buscaLinearAlvo(listaNum, numAlvo);
+
+if (index !== -1) {
+    console.log(`Numero encontrado em ${index}`);
+} else {
+    console.log(`Num não encontrado!`);
+}
